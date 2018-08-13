a5bf1e7641bb75ad7643d6a0cdae65f690c9e153
KingArthur <kingarthur@KingArthurdeMacBook-Pro.local>
2018-08-13 08:25:56 +0800

    update

--- client/js/signup.js
+++ client/js/signup.js
@@ -1,71 +1,81 @@
 var host = 'http://127.0.0.1:3000/';
 
 var signup_container = new Vue({
-    el: '#signup-container',
-    data: {
-      name_signin: '',
-      password_signin: '',
-      name: '',
-      password: '',
-      repassword: '',
-      firstname: '',
-      lastname: '',
-      birthday: '',
-      sexs: ['male', 'female'],
-      currentSex: 'male',
-      ages: ['1', '2', '3', '4', '5', '6', '7', '8',
-             '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'],
-      currentAge: '18',
-      wechart: '',
-      qq: '',
-      email: '',
-      contury: '',
-      address: '',
-      phone: '',
-      websize: '',
-      github: '',
-      bio: '',
-      commits: null,
+  el: '#signup-container',
+  data: {
+    name_signin: '',
+    password_signin: '',
+    name: '',
+    password: '',
+    repassword: '',
+    firstname: '',
+    lastname: '',
+    birthday: '',
+    sexs: ['male', 'female'],
+    currentSex: 'male',
+    ages: ['1', '2', '3', '4', '5', '6', '7', '8',
+      '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'],
+    currentAge: '18',
+    wechart: '',
+    qq: '',
+    email: '',
+    contury: '',
+    address: '',
+    phone: '',
+    websize: '',
+    github: '',
+    bio: '',
+    commits: null,
+    pagestate: '0'
+  },
+  methods: {
+    signin_index: function () {
+      this.pagestate = '1';
     },
-    methods: {
-      signin: function() {
-        var xhr = new XMLHttpRequest()
+    signup_index: function () {
+      this.pagestate = '2'
+    },
+    return_index: function() {
+      this.pagestate = '0'
+    },
+    signin: function () {
+      var xhr = new XMLHttpRequest()
 
-        var self = this
-        xhr.open('GET', host + 'signin?' + 'name=' + self.name_signin + '&password=' + self.password_signin, true)
-        
-        xhr.onload = function () {
-          self.commits = xhr.responseText
-        }
-        
-        xhr.send()
-      },
-      addUser: function () {
-        var xhr = new XMLHttpRequest()
+      var self = this
+      xhr.open('GET', host + 'signin?' + 'name=' + self.name_signin + '&password=' + self.password_signin, true)
 
-        /*
-        xhr.open('GET', host + 'name=' + this.name + '&password=' + this.password + '&firstname=' + 
-        this.firstname + '&lastname=' + this.lastname + '&birthday=' + this.birthday
-        + '&sex=' + this.currentSex + '&age=' + this.currentAge + '&wechart=' + this.wechart
-        + '&qq=' + this.qq + '&email=' + this.email + '&contury=' + this.contury
-        + '&address=' + this.address + '&phone=' + this.phone + '&websize=' + this.websize
-        + '&github=' + this.github + '&bio=' + this.bio, true)
-        xhr.send()
-        */
+      xhr.onload = function () {
+        self.commits = xhr.responseText
+      }
 
-        var self = this
-        xhr.open('GET', host + 'signup?' + 'name=' + self.name + '&password=' + self.password + '&firstname=' + 
+      xhr.send()
+    },
+    addUser: function () {
+      var xhr = new XMLHttpRequest()
+
+      /*
+      xhr.open('GET', host + 'name=' + this.name + '&password=' + this.password + '&firstname=' + 
+      this.firstname + '&lastname=' + this.lastname + '&birthday=' + this.birthday
+      + '&sex=' + this.currentSex + '&age=' + this.currentAge + '&wechart=' + this.wechart
+      + '&qq=' + this.qq + '&email=' + this.email + '&contury=' + this.contury
+      + '&address=' + this.address + '&phone=' + this.phone + '&websize=' + this.websize
+      + '&github=' + this.github + '&bio=' + this.bio, true)
+      xhr.send()
+      */
+
+      var self = this
+      xhr.open('GET', host + 'signup?' + 'name=' + self.name + '&password=' + self.password + '&firstname=' +
         self.firstname + '&lastname=' + self.lastname + '&birthday=' + self.birthday
         + '&sex=' + self.currentSex + '&age=' + self.currentAge + '&wechart=' + self.wechart
         + '&qq=' + self.qq + '&email=' + self.email + '&contury=' + self.contury
         + '&address=' + self.address + '&phone=' + self.phone + '&websize=' + self.websize
         + '&github=' + self.github + '&bio=' + self.bio, true)
-        
-        xhr.onload = function () {
-          self.commits = xhr.responseText
-        }
-        
-        xhr.send()
+
+      xhr.onload = function () {
+        self.commits = xhr.responseText
       }
+
+      xhr.send()
     }
-  })
+  }
+})
